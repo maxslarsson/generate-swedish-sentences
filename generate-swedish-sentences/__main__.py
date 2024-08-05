a677598fb64fb9412d140b86d92891100a8450ec
Max Larsson <maxslarsson@gmail.com>
2024-08-05 10:59:05 -0400

    Accidentally had deleted all code 4 years ago

---
+++ generate-swedish-sentences/__main__.py
@@ -0,0 +1,17 @@
+from typing import TextIO
+
+from pcfg import PCFG
+import os
+
+BASEPATH: str = os.path.dirname(__file__)
+
+f: TextIO
+with open(os.path.join(BASEPATH, "swedish_grammar.txt")) as f:
+    pcfg: PCFG = PCFG.fromstring(f.read())
+
+n: int = int(input("How many sentences do you want generated? "))
+sentence: str
+for sentence in pcfg.generate(n):
+    print()
+    print(sentence.capitalize()) 
+
